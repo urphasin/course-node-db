c7d33d63495cc399d0053e745ad7b4b159173bba
otito <otitoamuga@gmail.com>
2026-02-23 00:34:51 -0700

    p

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const { Client } = require("pg");
+
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "yourpassword",
+  database: "mydb",
+});
+
+async function run() {
+  await client.connect();
+  const res = await client.query("SELECT NOW()");
+  console.log(res.rows);
+  await client.end();
+}
